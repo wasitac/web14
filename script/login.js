6d642e4173125e77dc0de7c00b270392f5e3948c
jihong Yi <ghong817@gmail.com>
2021-10-27 17:44:01 +0900

    test

--- script/login.js
+++ script/login.js
@@ -1,8 +1,10 @@
+// 아이디 비밀번호 저장하고 불러오는거 몰라서 임시로 만든 userinfo
 const myinfo = {
     id: "myname",
     pw: "mypassword",
     name: "이지홍"
 };
+
 
 const infoCheck = (id, pw) => {
     if(id === myinfo.id){

261447e28a9bd82e61b56d51a88c6465d351f909
jihong Yi <ghong817@gmail.com>
2021-11-20 07:22:16 +0900

    .

--- db.sql
+++ db.sql
@@ -1,6 +1,4 @@
 CREATE TABLE `person` (
-    `name` TEXT NOT NULL,
-    `studentId` INT NOT NULL,
     `userId` VARCHAR(20) NOT NULL PRIMARY KEY,
     `userPw` TEXT NOT NULL ,
     `reservation` TIMESTAMP DEFAULT '0000-00-00 00:00:00'
@@ -8,18 +6,18 @@ CREATE TABLE `person` (
 
 
 INSERT INTO `person`
-(`name`, `studentId`, `phoneNumber`, `userId`,  `userPw`)
+(`userId`,  `userPw`)
 VALUES
-('이지홍', 2016010715, 01090853892, 'myname', 'mypassword');
+('myname', 'mypassword');
 
 
 UPDATE person
 SET
-    major = "cs"
-WHERE sutdentId = 2016010725;
+    reservation = 
+WHERE userId = edcqaz19;
 
 DROP DATABASE test;
 DROP TABLE person;
 
 SELECT *FROM person; 
-SELECT name, reservation FROM person WHERE studentId = 2016010725;
+SELECT name, reservation FROM person WHERE userId = edcqaz19;
